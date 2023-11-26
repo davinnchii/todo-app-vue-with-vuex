7339bb167a92a5f7d77545a6b4017697ca251631
daviinnchi <zaikoivan99@gmail.com>
2023-11-26 15:25:47 +0100

    Fix bugs when editing todo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import TodoHeader from "@/components/Header/TodoHeader.vue";
 import TodoList from "@/components/TodoList/TodoList.vue";
 import TodoFilters from "@/components/TodoFilters/TodoFilters.vue";
+import { useStore } from "vuex";
 
+const store = useStore();
 const filterParam = ref('all');
+const todos = computed(() => store.getters.getTodos,
+);
 
 </script>
 
 <template>
   <div class="todoapp">
-    <TodoHeader />
-    <TodoList :filterParam="filterParam" />
+    <TodoHeader/>
+    <TodoList v-if="todos.length" :filterParam="filterParam"/>
     <TodoFilters
         :filterParam="filterParam"
         :onParamChange="(newParam) => filterParam = newParam"
--- src/components/Header/TodoHeader.vue
+++ src/components/Header/TodoHeader.vue
@@ -1,9 +1,13 @@
 <script setup>
+import { mdiAccount, mdiAlertCircle } from "@mdi/js";
+
+const errorIcon = { mdiAlertCircle }
+
 import { ref } from "vue";
 import { useStore } from "vuex";
 
 const store = useStore();
-
+const hasError = ref(false);
 const newTodoTitle = ref('');
 
 function handleAddTodo(newTodoTitle) {
@@ -19,6 +23,15 @@ function handleAddTodo(newTodoTitle) {
 
 function handleSubmit(event) {
   event.preventDefault();
+  if (!newTodoTitle.value.trim()) {
+    hasError.value = true;
+
+    setTimeout(() => {
+      hasError.value = false;
+    }, 3000)
+
+    return;
+  }
   store.dispatch('addTodo', handleAddTodo(newTodoTitle.value))
   newTodoTitle.value = '';
 }
@@ -40,8 +53,25 @@ function handleSubmit(event) {
       />
     </form>
   </header>
+  <Transition>
+    <v-alert
+        v-if="hasError"
+        closable
+        text="Todo can't be empty!"
+        type="error"
+        variant="tonal"
+    />
+  </Transition>
 </template>
 
 <style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
 
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
 </style>
